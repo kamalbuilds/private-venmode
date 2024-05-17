fc879560f59f9161c7ffbfe3a741e616dc923348
Kamal Nayan <geniusamansingh@gmail.com>
2024-05-17 11:29:11 +0000

    frontend setup]

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,24 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { Sepolia}  from "@thirdweb-dev/chains";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThirdwebProvider 
+        activeChain={Sepolia} 
+        supportedChains={[Sepolia]}
+        clientId={process.env.NEXT_PUBLIC_THIRDWEB_KEY}
+        >
+      <ChakraProvider>
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </ChakraProvider>
+    </ThirdwebProvider>
+  );
 }
+
+export default MyApp;
